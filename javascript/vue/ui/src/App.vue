3d9037b46edcb4485be3d2b1b5997618134c846b
Robbie Hume <64663489+robbiehume@users.noreply.github.com>
2024-06-03 13:37:22 -0400

    Create App.vue

---
+++ javascript/vue/ui/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+        <MainView />
+        </div>
+  </template>
+  
+  <script setup>
+  import MainView from '@/components/MainView.vue';
+  
+
+  </script>
+  
+  <style>
+  </style>
+  
