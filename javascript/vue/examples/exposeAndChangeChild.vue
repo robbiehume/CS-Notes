a0d7670811101e56dd4ad5fd175e9e9f362c01ef
Robbie Hume <64663489+robbiehume@users.noreply.github.com>
2024-08-05 12:28:49 -0400

    Create exposeAndChangeChild.vue

---
+++ javascript/vue/examples/exposeAndChangeChild.vue
@@ -0,0 +1,60 @@
+// Parent
+
+<template>
+  <div>
+    <ChildComponent ref="child" />
+    <button @click="changeChildVariable">Change Child Variable</button>
+    <p>Child Variable: {{ child?.childVariable }}</p>
+    <button @click="invokeChildMethod">Invoke Child Method</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from './ChildComponent.vue';
+
+const child = ref(null);
+
+function changeChildVariable() {
+  if (child.value) {
+    child.value.childVariable = 'Changed value';
+  }
+}
+
+function invokeChildMethod() {
+  if (child.value) {
+    child.value.childMethod();
+  }
+}
+</script>
+
+
+// Child Component
+
+<template>
+  <div>
+    Child Component
+  </div>
+</template>
+
+<script setup>
+import { ref, defineExpose } from 'vue';
+
+const childVariable = ref('Initial value');
+
+function childMethod() {
+  console.log('Child method invoked!');
+}
+
+// Expose the variable and method
+defineExpose({
+  childVariable,
+  childMethod
+});
+
+
+</script>
+
+
+
+
