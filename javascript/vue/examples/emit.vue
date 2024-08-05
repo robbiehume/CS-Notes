3bb93293d2340f6568716415f7e2972b02d4732b
Robbie Hume <64663489+robbiehume@users.noreply.github.com>
2024-08-05 07:57:48 -0400

    Update emit.vue

--- javascript/vue/examples/emit.vue
+++ javascript/vue/examples/emit.vue
@@ -1,1 +1,16 @@
+// Parent
+...
+<ChildComponent @success="handleSuccess()" />
+...
+function handleSuccess(){
+  console.log('child event received')
+}
 
+
+// Child
+...
+const emit = defineEmits(['success'])
+...
+if(successful() == true){
+  emit('success')
+}
