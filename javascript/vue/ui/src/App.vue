fcab0cc4ab59cbd49b36a1ba956afebe2f39323d
Robbie Hume <64663489+robbiehume@users.noreply.github.com>
2024-08-15 10:45:21 -0400

    Update App.vue

--- javascript/vue/ui/src/App.vue
+++ javascript/vue/ui/src/App.vue
@@ -5,8 +5,19 @@
   </template>
   
   <script setup>
-  import MainView from '@/components/MainView.vue';
-  
+    import MainView from '@/components/MainView.vue';
+    import { useConfigStore } from '@/stores';
+    import { onMounted, ref } from 'vue';
+
+    const mountingComplete = ref(false);
+
+    onMounted(async () => {
+        const configStore = useConfigStore();
+        await configStore.fetchConfig();
+
+        mountingComplete.value = true;
+        document.getElementById('wrapper').style.minHeight = window.innerHeight - 85 + 'px';
+    });
 
   </script>
   
