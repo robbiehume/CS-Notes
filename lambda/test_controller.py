983472ee32ddf732380724b08410e4ea496b5e53
Robbie Hume <64663489+robbiehume@users.noreply.github.com>
2025-01-15 14:52:55 -0500

    Update test_controller.py

--- lambda/test_controller.py
+++ lambda/test_controller.py
@@ -1,26 +1,32 @@
-import utilities
 import traceback
+from http import HTTPStatus
+import json
 
-def handle_test(path, headers, data, parameters, cursor, query):
-    routes = {
-        '/query':  lambda: handle_query(cursor, query),
-        '/test':   lambda: handle_test(cursor),
-        '/insert': lambda: handle_insert(cursor)
-    }
+from utilities import get_all_rows_from_table, createError, createResponse, getJSONData
+from custom_router import router
 
-    try:
-        return routes.get(path, lambda: None)()
-    except:
-        print(traceback.format_exc())
-        return utilities.createError(HTTPStatus.BAD_REQUEST, 'Not a valid endpoint')
+# def handle_test(path, headers, data, parameters, cursor, query):
+#     routes = {
+#         '/query':  lambda: handle_query(cursor, query),
+#         '/test':   lambda: handle_test(cursor),
+#         '/insert': lambda: handle_insert(cursor)
+#     }
+
+#     try:
+#         return routes.get(path, lambda: None)()
+#     except:
+#         print(traceback.format_exc())
+#         return utilities.createError(HTTPStatus.BAD_REQUEST, 'Not a valid endpoint')
 
 
-def query_db(cursor, query):
-    try:
-        cursor.execute("""SELECT * FROM test_table WHERE val LIKE %(query)s """, {'query': query})
-        result_records = utilities.getJSONData(cursor)
-        return utilities.createResponse({'results': result_records})
+
+@router.get('/api/query')
+def query_db(request):
+    # try:
+    request.cursor.execute("""SELECT * FROM test_table WHERE val LIKE %(query)s """, {'query': request.query})
+    result_records = utilities.getJSONData(cursor)
+    return utilities.createResponse({'results': result_records})
         
-    except Exception as e:
-        print(traceback.format_exc())
-        return utilities.createError(HTTPStatus.INTERNAL_SERVER_ERROR, "Error querying database")
+    # except Exception as e:
+    #     print(traceback.format_exc())
+    #     return utilities.createError(HTTPStatus.INTERNAL_SERVER_ERROR, "Error querying database")
