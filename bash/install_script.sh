3b62f7fadcf58896b933b1f95516dd3bdb1edd31
Robbie Hume <64663489+robbiehume@users.noreply.github.com>
2025-03-17 10:12:32 -0400

    Update install_script.sh

--- bash/install_script.sh
+++ bash/install_script.sh
@@ -11,20 +11,20 @@ error_handler() {
 # Trap errors and call error_handler with the line number
 trap 'error_handler $LINENO' ERR
 
-user=$(whoami)
-
-if [[ $user != 'root' ]]; then
+if [[ "$EUID" -ne 0 ]]; then
    echo -e "Script must be run as root\nPlease try running it again with sudo: \"sudo bash install_script.sh\""
    exit
 fi
 
 get_service_status (){
     stat=$(sudo systemctl status $1)
-    if [[ $stat == *"inactive"* ]]; then
-        echo "$stat" | grep -E --color "\b(inactive|dead)\b|$"
+    echo "-----------------------------"
+    if [[ $stat == *"inactive"* || $stat == *"failed"* ]]; then
+        echo "$stat" | grep -E --color "\b(inactive|dead|failed)\b|$"
     else
         export GREP_COLORS='ms=01;32'
         echo "$stat" | grep -E --color "\b(active|running)\b|$"
     fi
     export GREP_COLORS=''
+    echo "-----------------------------"
 }
